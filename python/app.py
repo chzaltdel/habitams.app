47aaaa0424f9a48c44cc45ed884a1fd7abef8979
chzaltdel <francisandres0705@gmail.com>
2026-03-11 20:11:22 +0800

    Initial commit

--- python/app.py
+++ python/app.py
@@ -5,13 +5,24 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 
 parent_dir = os.path.dirname(current_dir)
 
-template_dir = os.path.join(parent_dir, 'web')
+web_dir = os.path.join(parent_dir, 'web')
 
-app = Flask(__name__, template_folder=template_dir, static_folder=template_dir)
+app = Flask(__name__, 
+            template_folder=web_dir, 
+            static_folder=os.path.join(web_dir, 'styles'),
+            static_url_path='/styles')
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def home(): 
+    return render_template('home.html')
+
+@app.route('/habits')
+def habits():
+    return render_template('habits.html')
+
+@app.route('/collect')
+def collect():
+    return render_template('collect.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
